a942ed84bcee6a4b5268f8c02e9ede2b9ce5d67d
abhishek636 <bhattabhishek636@gmail.com>
2025-05-05 11:44:10 +0530

    changes

--- src/app/components/MarqueeBanner.tsx
+++ src/app/components/MarqueeBanner.tsx
@@ -1,5 +1,7 @@
 const MarqueeBanner = ({ text }: { text: string }) => (
-    <div className="absolute top-0 w-full bg-[#FFFFFF29] flex items-center gap-4 p-2 backdrop-blur-3xl whitespace-nowrap overflow-hidden">
+    <div className="absolute top-0 w-full bg-[#FFFFFF29] flex items-center gap-4 p-2 backdrop-blur-3xl whitespace-nowrap overflow-hidden"
+      
+    >
       {[...Array(3)].map((_, i) => (
         <div key={i} className="animate-marquee text-white">
           {text} &nbsp;&bull;&nbsp; {text} &nbsp;&bull;&nbsp; {text}
