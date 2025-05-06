004e93c1ae3eca0590708545d628ba2c7284ef75
abhishek636 <bhattabhishek636@gmail.com>
2025-05-06 23:08:29 +0530

    animation

--- src/app/components/CardGrid.tsx
+++ src/app/components/CardGrid.tsx
@@ -17,7 +17,7 @@ const CardGrid = ({ cards, onCardClick, hide }: CardGridProps) => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          transition={{ duration: 1, delay: 0.5, ease: 'easeInOut' }} // <-- Added delay here
+          transition={{ duration: 0.7, delay: 0.5, ease: 'easeInOut' }} // <-- Added delay here
           className="absolute xl:bottom-34 sm:bottom-56 bottom-25 w-full flex justify-center sm:gap-5 gap-3 pointer-events-auto"
         >
           {cards.map((card) => (
@@ -31,6 +31,7 @@ const CardGrid = ({ cards, onCardClick, hide }: CardGridProps) => {
                 alt={card.title}
                 width={127}
                 height={127}
+                loading="eager"
                 className="w-20 h-20 md:w-32 lg:w-full lg:h-full backdrop_custom rounded-3xl"
               />
               <p className="sm:text-xl mt-3.5 text-[#CDDDDE] font-bold uppercase rawpixel">
