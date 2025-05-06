38adba0105e0440c0662bff5769d6352b2262f39
abhishek636 <bhattabhishek636@gmail.com>
2025-05-06 23:00:39 +0530

    animation

--- src/app/components/CardGrid.tsx
+++ src/app/components/CardGrid.tsx
@@ -14,10 +14,10 @@ const CardGrid = ({ cards, onCardClick, hide }: CardGridProps) => {
       {!hide && (
         <motion.div
           key="card-grid"
-          initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: 50 }}
-          transition={{ duration: 0.6, ease: 'easeInOut' }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 1, delay: 0.5, ease: 'easeInOut' }} // <-- Added delay here
           className="absolute xl:bottom-34 sm:bottom-56 bottom-25 w-full flex justify-center sm:gap-5 gap-3 pointer-events-auto"
         >
           {cards.map((card) => (
