33cac1cd93ad09fb233e3a17134a9764311def96
abhishek636 <bhattabhishek636@gmail.com>
2025-05-06 23:24:26 +0530

    use img tag

--- src/app/components/CardGrid.tsx
+++ src/app/components/CardGrid.tsx
@@ -13,7 +13,11 @@ const CardGrid = ({ cards, onCardClick, hide }: CardGridProps) => {
     <AnimatePresence>
       {!hide && (
         <motion.div
-          
+          key="card-grid"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.7, delay: 0.5, ease: 'easeInOut' }} // <-- Added delay here
           className="absolute xl:bottom-34 sm:bottom-56 bottom-25 w-full flex justify-center sm:gap-5 gap-3 pointer-events-auto"
         >
           {cards.map((card) => (
@@ -22,14 +26,13 @@ const CardGrid = ({ cards, onCardClick, hide }: CardGridProps) => {
               className="flex flex-col items-center hover:scale-110 transition-transform cursor-pointer"
               onClick={() => onCardClick(card)}
             >
-              <Image
-                src={card.imageSrc}
-                alt={card.title}
-                width={127}
-                height={127}
-                loading="eager"
-                className="w-20 h-20 md:w-32 lg:w-full lg:h-full backdrop_custom rounded-3xl"
-              />
+              <img
+  src={card.imageSrc}
+  alt={card.title}
+  width={127}
+  height={127}
+  className="w-20 h-20 md:w-32 lg:w-full lg:h-full backdrop_custom rounded-3xl"
+/>
               <p className="sm:text-xl mt-3.5 text-[#CDDDDE] font-bold uppercase rawpixel">
                 {card.title}
               </p>
